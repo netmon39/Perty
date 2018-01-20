8a180ddb8a6ebbc51ec4b622811f17bb41fd8d5f
netmon39 <m.netipol@gmail.com>
2018-01-21 02:46:09 +0700

    Yo.

---
+++ app/src/main/java/com/example/netipol/perty/AccountActivity.java
@@ -0,0 +1,13 @@
+package com.example.netipol.perty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
